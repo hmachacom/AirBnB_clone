e25718e8f12a138c065fc88ba7eea1b3dfe3b030
caritoespicaita <caritoespicaita@hotmail.com>
2022-03-03 00:41:22 -0500

    update task 7.

---
+++ models/user.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+from models.base_model import BaseModel
+
+class User(BaseModel):
+    """ inherits Class User from BaseModel """
+    email = " "
+    password = " "
+    first_name  = " "
+    last_name= " "
