a28ebc87e3028be30bb9181c1ed02dc5b391f9aa
caritoespicaita <caritoespicaita@hotmail.com>
2022-03-04 17:30:17 -0500

    Update tests

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Unittest for BaseModel()
+"""
+import unittest
+from models.base_model import BaseModel
+from uuid import uuid4
+from datetime import datetime, date
+
+
+
+class TestsBaseModel(unittest.TestCase):
+
+    def test_attributes(self):
+        """ verify attributes """
+        my_object = BaseModel()
+        my_object2 = BaseModel()
+        my_object.name = "Holbiland"
+        my_object.my_number = 29
+        self.assertFalse(my_object.id == my_object2.id)
+        self.assertTrue(date.today() == my_object.created_at.date())
+        self.assertTrue(date.today() == my_object.updated_at.date())
+    
+    def test_dictionary(self):
+        my_object = BaseModel()
+        self.assertEqual(type(my_object.__dict__), dict)
+
+if __name__ == '__main__':
+    unittest.main()
