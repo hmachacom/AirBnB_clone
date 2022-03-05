c1e5180859115c1cb832fe2cbab9ac09abbe5837
caritoespicaita <caritoespicaita@hotmail.com>
2022-03-05 09:42:48 -0500

    fix pycodestyle

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -7,17 +7,16 @@ from uuid import uuid4
 from datetime import datetime, date
 
 
-
 class TestsBaseModel(unittest.TestCase):
-
 
     def test_instancia(self):
         """ test of instance"""
         my_object = BaseModel()
-        self.assertEqual(str(type(my_object)), "<class 'models.base_model.BaseModel'>")
+        self.assertEqual(str(type(my_object)),
+                         "<class 'models.base_model.BaseModel'>")
         self.assertIsInstance(my_object, BaseModel)
         self.assertTrue(issubclass(type(my_object), BaseModel))
-   
+
     def test_attributes(self):
         """ verify attributes """
         my_object = BaseModel()
@@ -27,10 +26,11 @@ class TestsBaseModel(unittest.TestCase):
         self.assertFalse(my_object.id == my_object2.id)
         self.assertTrue(date.today() == my_object.created_at.date())
         self.assertTrue(date.today() == my_object.updated_at.date())
-    
+
     def test_dictionary(self):
         my_object = BaseModel()
         self.assertEqual(type(my_object.__dict__), dict)
+
 
 if __name__ == '__main__':
     unittest.main()
